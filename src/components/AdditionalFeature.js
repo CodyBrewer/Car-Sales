895516021fb80bb39e78a9399cb2f024a8de4fc5
Cody Brewer <brewer.cody.m@gmail.com>
2020-02-05 12:34:31 -0700

    added addFeature dispatch to additionalFeatures

--- src/components/AdditionalFeature.js
+++ src/components/AdditionalFeature.js
@@ -1,13 +1,36 @@
-import React from 'react';
+import React from "react";
+import { connect } from "react-redux";
+import { addFeature } from "../store/actions";
 
 const AdditionalFeature = props => {
+  const { feature, addFeature } = props;
+
   return (
     <li>
       {/* Add an onClick that will let you add a feature to your car */}
-      <button className="button">Add</button>
-      {props.feature.name} (+{props.feature.price})
+      <button
+        className="button"
+        onClick={() => {
+          addFeature(feature);
+        }}
+      >
+        Add
+      </button>
+      {feature.name} (+{feature.price})
     </li>
   );
 };
 
-export default AdditionalFeature;
+const mapStateToProps = state => {
+  return {};
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    addFeature: feature => {
+      dispatch(addFeature(feature));
+    }
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(AdditionalFeature);
