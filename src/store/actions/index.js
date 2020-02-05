bcb442639476a8245911bd09d64bad37101b0434
Cody Brewer <brewer.cody.m@gmail.com>
2020-02-05 12:25:25 -0700

    create addFeature and removeFeature actions

--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -1,1 +1,11 @@
 import { ADD_FEATURE, REMOVE_FEATURE } from "./types";
+
+export const addFeature = feature => ({
+  type: ADD_FEATURE,
+  payload: feature
+});
+
+export const removeFeature = feature => ({
+  type: REMOVE_FEATURE,
+  payload: feature
+});
