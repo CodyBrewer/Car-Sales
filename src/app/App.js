5c1107478ac386fc553c9eed8a1955abf45e82ba
Cody Brewer <brewer.cody.m@gmail.com>
2020-02-05 12:33:53 -0700

    removed unused fns from app.js

--- src/app/App.js
+++ src/app/App.js
@@ -7,13 +7,6 @@ import Total from "../components/Total";
 import { connect } from "react-redux";
 
 const App = props => {
-  const removeFeature = item => {
-    // dispatch an action here to remove an item
-  };
-
-  const buyItem = item => {
-    // dipsatch an action here to add an item
-  };
   return (
     <div className="boxes">
       <div className="box">
